e8f2f27cf2b610569690bb76039dd84a91c4b48b
rgomes <iamrubengomes@gmail.com>
2020-03-07 19:10:02 +0100

    gamemanager, singleton, poi quest, etc

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text questText;
+
+    private void Awake ()
+    {
+        Assert.IsNotNull(questText);
+    }
+
+    private void Update()
+    {
+        updateQuest();
+    }
+
+    public void updateQuest()
+    {
+        questText.text = "Quete #" + GameManager.Instance.CurrentPlayer.Quest.ToString();
+    }
+}
