64ca787b2129c351e465cab33a7fd1130ea38d5a
Sarah Steeman <sarah.steeman@student.uclouvain.be>
2020-03-07 13:37:53 +0100

    scene differente pour la liste des quetes ainsi que boutons pour y acceder

---
+++ Assets/Scripts/ButtonQuestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonQuestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoQuest(string Level) {
+        SceneManager.LoadScene("QuestListScreen", LoadSceneMode.Single); 
+    }
+}
