0696ee0f1a005157ad8d1c2d8ec8e8387eb09d8a
rgomes <iamrubengomes@gmail.com>
2020-03-08 15:30:12 +0100

    questfactory

---
+++ Assets/Scripts/QuestFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+
+public class QuestFactory : MonoBehaviour
+{
+    [SerializeField] private GameObject POIQuest;
+    [SerializeField] AbstractMap map;
+    [SerializeField] float height;
+
+    private string[] availableQuestsCoordinates =  new string[] {
+        "50.669966,4.6161605"
+    };
+
+    void Start ()
+    {
+        SpawnQuestsFromCoordinates(0);
+    }
+
+    private void SpawnQuestsFromCoordinates(int i)
+    {
+        Vector2d[] locations = new Vector2d[availableQuestsCoordinates.Length];
+
+        locations[i] = Conversions.StringToLatLon(availableQuestsCoordinates[i]);
+
+        var instance = Instantiate(POIQuest);
+        instance.transform.localPosition = map.GeoToWorldPosition(locations[i], true);
+
+        Debug.Log(map.GeoToWorldPosition(locations[i], true));
+
+        instance.transform.localPosition = new Vector3(instance.transform.localPosition.x, instance.transform.localPosition.y + height, instance.transform.localPosition.z);
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Factories;
+using Mapbox.Unity.Utilities;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    AbstractMap _map;
+
+    [SerializeField]
+    [Geocode]
+    string[] _locationStrings;
+    Vector2d[] _locations;
+
+    [SerializeField]
+    float _spawnScale = 100f;
+
+    [SerializeField]
+    float _height = 10f;
+
+    [SerializeField]
+    GameObject _markerPrefab;
+
+
+
+    List<GameObject> _spawnedObjects;
+
+    void Start()
+    {
+        _locations = new Vector2d[_locationStrings.Length];
+        _spawnedObjects = new List<GameObject>();
+        for (int i = 0; i < _locationStrings.Length; i++)
+        {
+            var locationString = _locationStrings[i];
+            _locations[i] = Conversions.StringToLatLon(locationString);
+            print(_locations[0][0]);
+            var instance = Instantiate(_markerPrefab);
+            instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+            print(instance.transform.localPosition.z + 10);
+            instance.transform.localPosition = new Vector3(instance.transform.localPosition.x, instance.transform.localPosition.y + _height, instance.transform.localPosition.z);
+
+            instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+            _spawnedObjects.Add(instance);
+        }
+    }
+
+    private void Update()
+    {
+        int count = _spawnedObjects.Count;
+        for (int i = 0; i < count; i++)
+        {
+            var spawnedObject = _spawnedObjects[i];
+            var location = _locations[i];
+            spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+            spawnedObject.transform.localPosition = new Vector3(spawnedObject.transform.localPosition.x, spawnedObject.transform.localPosition.y + _height, spawnedObject.transform.localPosition.z);
+
+            spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+        }
+    }
+}
