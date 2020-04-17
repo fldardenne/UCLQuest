15546902c4760d22b0d454c5c02b8bd4e715c24b
Florent Dardenne <dardenneflorent@outlook.com>
2020-04-17 23:39:10 +0200

    Merged manually with master

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject Inventory;
+
+    public void Show()
+    {
+        if (Inventory != null)
+        {
+            Inventory.SetActive(true);
+        }
+    }
+
+    public void Hide()
+    {
+        if (Inventory != null)
+        {
+            Inventory.SetActive(false);
+        }
+    }
+}
